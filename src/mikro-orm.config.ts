2db5c89fb1a44202805c28550e2baf4804d92ce6
Olawale Otubu <waleoxy@gmail.com>
2022-08-05 14:29:43 +0100

    feat: intro of migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,14 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+  migrations: {
+    tableName: "mikro_orm_migrations", // name of database table with log of executed transactions
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    glob: "!(*.d).{js,ts}", // how to match migration files (all .js and .ts files, but not .d.ts)
+  },
   entities: [Post],
   dbName: "gospelShareDb",
   type: "postgresql",
